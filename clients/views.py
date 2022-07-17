03bb592847fadf713b5f58dc44fb7b02fbdf0474
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-17 11:49:58 -0300

    Adequando a view para o método POST

--- clients/views.py
+++ clients/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .forms import RegisterForm
 
 def register_view(request):
+    if request.Post:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
     form = RegisterForm
     return render(request, 'clients/pages/register_author.html', context={
         'form': form,
