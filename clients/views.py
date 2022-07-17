6b7a3d3e3db6275a71da4a2a12e8f2e9c574f6c7
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-17 11:55:11 -0300

    Separando método view e create

--- clients/views.py
+++ clients/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import Http404
 
 def register_view(request):
-    if request.Post:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
-    form = RegisterForm
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'clients/pages/register_author.html', context={
         'form': form,
     })
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    POST = request.POST
+    request.session['register_from_data'] = POST
+    form = RegisterForm(POST)
+    return redirect('register')
