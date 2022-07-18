88a6de99437f071de1efd0cead4a04899d648912
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-18 16:40:05 -0300

    Commit

--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,11 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from clients.forms import LoginForm
 from clients.forms.register_form import RegisterForm
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
 
 def register_view(request):
@@ -26,13 +30,50 @@ def register_create(request):
         user.save()
         messages.success(request, 'Usuário criado. Faça o login.')
         del(request.session['register_form_data'])
+        return redirect(reverse('login'))
 
     return redirect('register')
 
 
 def login_view(request):
-    return render(request, 'authors/pages/login.html')
+    form = LoginForm()
+    return render(request, 'clients/pages/login.html', {
+        'form': form,
+        'form_action': reverse('login_create')
+    })
 
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Your are logged in.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(login_url)
+
+
+@login_required(login_url='login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('login'))
+
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('login'))
+
+    logout(request)
+    return redirect(reverse('login'))
