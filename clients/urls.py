4dece6d460e078f1e5fa0d8cf08dbd08ca25ea1c
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-18 09:36:52 -0300

    Commit

--- clients/urls.py
+++ clients/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path('register/create', views.register_create, name='create')
+    path('create/', views.register_create, name='create')
 ]
