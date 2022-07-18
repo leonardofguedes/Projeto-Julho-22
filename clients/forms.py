4af140fe24ed94eb5e107ca423be730eec785e76
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-18 15:28:45 -0300

    Commit

--- clients/forms.py
+++ clients/forms.py
@@ -1,8 +1,59 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
+
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'A senha precisa de uma letra maiúscula, uma minúscula e um número.'
+            'Ela não pode ter menos que 8 caracteres',
+        ),
+            code='invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['first_name'], 'Ex: Pedro')
+        add_placeholder(self.fields['last_name'], 'Ex: Silva')
+        add_placeholder(self.fields['username'], 'Username')
+        add_placeholder(self.fields['email'], 'Seu e-mail')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Sua senha'
+        }),
+        error_messages={
+            'required': 'A senha pode não ser vazia'
+        },
+        help_text=(
+            'A senha precisa de uma letra maiúscula, uma minúscula e um número.'
+            'Ela não pode ter menos que 8 caracteres'
+        ),
+        validators=[strong_password]
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repita a senha'
+        })
+    )
+
+
     class Meta:
         model= User
         fields = [
@@ -12,9 +63,75 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
             ]
+        labels = {
+            'first_name': 'Primeiro nome',
+            'last_name': 'Último nome',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Senha',
+        }
+        help_texts = {
+            'email': 'O email precisa ser válido'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
         widgets = {
             'password': forms.PasswordInput(attrs={
                 'placeholder': 'Digite sua senha aqui'
             })
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite %(pipoca)s no campo password',
+                code='invalid',
+                params={'pipoca': '"atenção"'}
+            )
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+        if 'John Doe' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo first name',
+                code='invalid',
+                params={'value': '"John Doe"'}
+            )
+        return data
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid',
+            )
+
+        return email
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                'Password and password2 must be equal',
+                code='invalid'
+            )
+            raise ValidationError({
+                'password': password_confirmation_error,
+                'password2': [
+                    password_confirmation_error,
+                ],
+            })
 
--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import Http404
@@ -12,7 +13,16 @@ def register_view(request):
 def register_create(request):
     if not request.POST:
         raise Http404()
+
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        messages.success(request, 'Usuário criado. Faça o login.')
+        del(request.session['register_form_data'])
+
     return redirect('register')
