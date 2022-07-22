5ba447a6c873328c77cc481809eb609ef92d3aa7
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-22 18:33:49 -0300

    Invertendo ordem dos paths

--- setup/urls.py
+++ setup/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('petstore.urls')),
     path('users/', include('clients.urls')),
+    path('', include('petstore.urls')),
+
 ]
